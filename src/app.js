a9a7c0607d3012c7acfcf70797e5909816fd6077
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-04-13 09:40:29 +0100

    new repository created
    All data and files transferred over from old folder

---
+++ src/app.js
@@ -0,0 +1,22 @@
+//function bine(){
+//let nume = prompt ("Cum te numesti?");
+//{alert("Bine ai venit in gradina noastra, " + nume + "!");
+//}
+//}
+
+//bine();
+
+//function order(){
+//let comanda = prompt ("Vrei sa comanzi legume?")
+//if (comanda==="da"){
+//    alert("Te rugam lasa ne un mesaj cu cantitatea de legume dorite si datele tale de contact, echipa noastra te va contacta curand!")}
+//else {
+//   alert(
+//      "Te rugam lasa ne un mesaj cu cantitatea de rasaduri dorite si datele tale de contact, echipa noastra te va contacta curand! "
+//   );
+//}
+
+//}
+
+//let orderButton = document.querySelector("button");
+//orderButton.addEventListener("click", order);//
